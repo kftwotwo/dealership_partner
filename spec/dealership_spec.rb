76df6303ea6b4e845f36d8444adda21558ff37cc
Diego Suarez and Kevin Finley <kevin@>
2016-11-30 10:07:18 -0800

    add 13th spec

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -2,16 +2,35 @@ require('rspec')
 require('dealership')
 
 describe(Dealership) do
-  describe('info attributes') do
+  describe('#initialize') do
     it "will get the info from dealership" do
       test_dealer = Dealership.new("Diego")
       expect(test_dealer.name()).to(eq("Diego"))
       expect(test_dealer.id()).to(eq(1))
       expect(test_dealer.cars()).to(eq([]))
+    end
+  end
+  describe('#save') do
+    it('will return the saved dealership') do
+      test_dealer = Dealership.new("Diego")
       test_dealer.save()
       expect(Dealership.all()).to(eq([test_dealer]))
+    end
+  end
+
+  describe('.all') do
+    it ('will display the array') do
+      test_dealer = Dealership.new("Diego").save()
+      expect(Dealership.all()).to(eq(test_dealer))
+    end
+  end
+
+  describe('.clear') do
+    it ('will clear the dealership array') do
+      Dealership.new("Diego").save()
       Dealership.clear()
       expect(Dealership.all()).to(eq([]))
     end
   end
+
 end
